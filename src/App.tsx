69a69acefd80b1a9a756c4ad7d85f9d524730e95
money <hualigushi@163.com>
2020-10-05 11:45:40 +0800

    feat: button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Button,{ButtonType,ButtonSize} from './components/Button/button'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button autoFocus>Hello</Button>
+      <Button disabled={true}>Disabled Button</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Large Primary</Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Small Danger</Button>
+      <Button btnType={ButtonType.Link} href="www.baidu.com">Baidu Link</Button>
+      <Button disabled={true} btnType={ButtonType.Link} href="www.baidu.com">Disabled Link</Button>
     </div>
   );
 }
